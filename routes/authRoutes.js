90531948950db1395d470d8e9336c21584c8b37c
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2024-04-23 23:39:33 +0500

    updates in backend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -46,7 +46,178 @@ router.post("/signup", async (req, res) => {
       from: process.env.EMAIL,
       to: user.email,
       subject: "Verify your email",
-      text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
+      // text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
+      html: `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <title>Verify your OTP</title>
+      
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
+        </head>
+        <body
+          style="
+            margin: 0;
+            font-family: 'Poppins', sans-serif;
+            background: #ffffff;
+            font-size: 14px;
+          "
+        >
+          <div
+            style="
+              max-width: 680px;
+              margin: 0 auto;
+              padding: 45px 30px 60px;
+              background: #ffffff;
+              background-image: url(https://i.ibb.co/dj7cT9z/seamless-gold-rhombus-grid-pattern-black-background-53876-97589.jpg);
+              background-repeat: no-repeat;
+              background-size: 800px 452px;
+              background-position: top center;
+              font-size: 14px;
+              color: #434343;
+            "
+          >
+            <header>
+              <table style="width: 100%;">
+                <tbody>
+                  <tr style="height: 0;">
+                    <td>
+                      <img
+                        alt=""
+                        src="https://i.ibb.co/cwfy96W/synthseer.png"
+                        height="30px"
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </header>
+      
+            <main>
+              <div
+                style="
+                  margin: 0;
+                  margin-top: 70px;
+                  padding: 92px 30px 115px;
+                  background: #ffffff;
+                  border-radius: 30px;
+                  text-align: center;
+                "
+              >
+                <div style="width: 100%; max-width: 489px; margin: 0 auto;">
+                  <h1
+                    style="
+                      margin: 0;
+                      font-size: 24px;
+                      font-weight: 500;
+                      color: #1f1f1f;
+                    "
+                  >
+                    Your OTP
+                  </h1>
+                  <p
+                    style="
+                      margin: 0;
+                      margin-top: 17px;
+                      font-size: 16px;
+                      font-weight: 500;
+                    "
+                  >
+                    Hello,
+                  </p>
+                  <p
+                    style="
+                      margin: 0;
+                      margin-top: 17px;
+                      font-weight: 500;
+                      letter-spacing: 0.56px;
+                    "
+                  >
+                    Thank you for choosing Synthseer. Use the following OTP
+                    to complete the procedure to change your email address. OTP is
+                    valid for
+                    <span style="font-weight: 600; color: #1f1f1f;">5 minutes</span>.
+                    Do not share this code with others, including Synthseer
+                    employees.
+                  </p>
+                  <p
+                    style="
+                      margin: 0;
+                      margin-top: 60px;
+                      font-size: 40px;
+                      font-weight: 600;
+                      letter-spacing: 25px;
+                      color: #9034ca;
+                    "
+                  >
+                    ${otp}
+                  </p>
+                </div>
+              </div>
+      
+              <p
+                style="
+                  max-width: 400px;
+                  margin: 0 auto;
+                  margin-top: 90px;
+                  text-align: center;
+                  font-weight: 500;
+                  color: #8c8c8c;
+                "
+              >
+                Need help? Ask at
+                <a
+                  href="mailto:synthseer@gmail.com"
+                  style="color: #9034ca; text-decoration: none;"
+                  >synthseer@gmail.com</a
+                >
+                or visit our
+                <a
+                  href=""
+                  target="_blank"
+                  style="color: #9034ca; text-decoration: none;"
+                  >Help Center</a
+                >
+              </p>
+            </main>
+      
+            <footer
+              style="
+                width: 100%;
+                max-width: 490px;
+                margin: 20px auto 0;
+                text-align: center;
+                border-top: 1px solid #e6ebf1;
+              "
+            >
+              <p
+                style="
+                  margin: 0;
+                  margin-top: 40px;
+                  font-size: 16px;
+                  font-weight: 600;
+                  color: #434343;
+                "
+              >
+              SYNTHSEER              </p>
+              <p style="margin: 0; margin-top: 8px; color: #434343;">
+                Address 540, City, State.
+              </p>
+              <p style="margin: 0; margin-top: 16px; color: #434343;">
+                Copyright © 2025 Synthseer. All rights reserved.
+              </p>
+            </footer>
+          </div>
+        </body>
+      </html>
+      
+`,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
@@ -104,7 +275,7 @@ router.post("/verify-otp", async (req, res) => {
       (err, token) => {
         if (err) throw err;
         res.json({ token });
-      },
+      }
     );
   } catch (err) {
     console.error(err.message);
@@ -146,7 +317,7 @@ router.post("/login", async (req, res) => {
             email: user.email,
           },
         });
-      },
+      }
     );
   } catch (err) {
     console.error(err.message);
